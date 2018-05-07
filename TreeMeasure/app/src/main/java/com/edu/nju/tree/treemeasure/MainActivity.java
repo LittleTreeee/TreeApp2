fa36cf18d1c85afb16043f39ed2be56352c213ab
刘钰 <liuyu@appledeMacBook-Pro.local>
2018-05-07 18:53:33 +0800

    移代码

--- TreeMeasure/app/src/main/java/com/edu/nju/tree/treemeasure/MainActivity.java
+++ TreeMeasure/app/src/main/java/com/edu/nju/tree/treemeasure/MainActivity.java
@@ -5,8 +5,6 @@ import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import org.opencv.android.BaseLoaderCallback;
@@ -30,15 +28,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tutorial();
-            }
-        });
+//        setContentView(R.layout.activity_main);
+//
+//        Button button = (Button)findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                tutorial();
+//            }
+//        });
+        //整个项目只有一个activity，启动以后都是activity上fragment之间的切换
+        setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
 
     }
 
