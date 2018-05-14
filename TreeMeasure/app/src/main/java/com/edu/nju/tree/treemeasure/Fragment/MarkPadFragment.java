08b5317bf3624c268cb717df8242edf828dcc7f1
刘钰 <liuyu@appledeMacBook-Pro.local>
2018-05-14 22:30:12 +0800

    保留两位小数

--- TreeMeasure/app/src/main/java/com/edu/nju/tree/treemeasure/Fragment/MarkPadFragment.java
+++ TreeMeasure/app/src/main/java/com/edu/nju/tree/treemeasure/Fragment/MarkPadFragment.java
@@ -20,6 +20,8 @@ import com.edu.nju.tree.treemeasure.utils.ImageProcess;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -99,11 +101,12 @@ public class MarkPadFragment extends Fragment {
             public void onClick(View v) {
                 //todo 保存
 //                saveImage();
-                Toast.makeText(mContext, "开始计算...", Toast.LENGTH_LONG).show();
+                Toast.makeText(mContext, "开始计算...", Toast.LENGTH_SHORT).show();
 
                 double treeWidth = ImageProcess.treeWidth(bitmap);
 
-                Toast.makeText(mContext, "胸径: "+ treeWidth, Toast.LENGTH_LONG).show();
+                BigDecimal bg = new BigDecimal(treeWidth);
+                Toast.makeText(mContext, "胸径: "+ bg.setScale(2), Toast.LENGTH_LONG).show();
                 
 
             }
